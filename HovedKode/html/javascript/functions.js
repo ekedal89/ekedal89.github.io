88afa13bc97ede5bb43018fbd9965e596d3decf9
sUmi7ro <143796540+sUmi7ro@users.noreply.github.com>
2024-03-13 13:58:26 +0100

    iijoijpoløl,løkjn

---
+++ HovedKode/html/javascript/functions.js
@@ -0,0 +1,28 @@
+       function booking(){
+           window.location.href = "Booking.html";
+       }
+
+       function medlemskap(){
+           window.location.href = "Medlemskap.html";
+       }
+
+       function kontaktoss(){
+           window.location.href = "KontaktOss.html";
+       }
+
+       function arrangement(){
+           window.location.href = "i.arragement.html";
+       }
+
+       function omOss(){
+           window.location.href = "OmOss.html";
+       }
+
+       function minSide(){
+           window.location.href = "MinSide.html";
+       }
+
+       function forside(){
+           window.location.href = "Forside.html";
+       }
+
